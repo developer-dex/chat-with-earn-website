c4ddec8cf54564f7fe78a4b86c318bb185cd9570
AryanPatel1007 <aryan.patel@avestatechnologies.com>
2025-02-12 22:52:24 +0530

    static image set

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import { Suspense } from "react";
-import { BrowserRouter } from "react-router-dom";
-import ErrorBoundary from "./hoc/ErrorBoundary";
-import AppRoutes from "./routes/AppRoutes";
-import ToastNotification from "./components/common/ToastNotification";
-import { config } from "./config/config";
+// import { Suspense } from "react";
+// import { BrowserRouter } from "react-router-dom";
+// import ErrorBoundary from "./hoc/ErrorBoundary";
+// import AppRoutes from "./routes/AppRoutes";
+// import ToastNotification from "./components/common/ToastNotification";
+// import { config } from "./config/config";
 
 function App() {
 
   return (
-    <>
-      <BrowserRouter basename={config.baseName}>
-        <ErrorBoundary>
-          <Suspense fallback={<div>Loading...</div>}>
-            <AppRoutes />
-            <ToastNotification />
-          </Suspense>
-        </ErrorBoundary>
-      </BrowserRouter>
-    </>
+    // <>
+    //   <BrowserRouter basename={config.baseName}>
+    //     <ErrorBoundary>
+    //       <Suspense fallback={<div>Loading...</div>}>
+    //         <AppRoutes />
+    //         <ToastNotification />
+    //       </Suspense>
+    //     </ErrorBoundary>
+    //   </BrowserRouter>
+    // </>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <img 
+      src="/Meet-With-Money.jpg" 
+      alt="Coming Soon" 
+      style={{ maxWidth: '50%', height: 'auto' }} 
+    />
+  </div>
   )
 }
 
